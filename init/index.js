6e0f07f8de8a50bd44bcd207366aa53d4dccc3d5
mohddaniyal <mohddaniyal.12b@gmail.com>
2026-06-08 17:33:57 +0530

    feat: initialize database with seed data and default owner field assignments

--- init/index.js
+++ init/index.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-const MONGO_URL = "mongodb+srv://user:YOUR_PASSWORD@cluster0.970bv3p.mongodb.net/?retryWrites=true&w=majority";
+const MONGO_URL = "mongodb+srv://username:Wanderlust123@cluster0.970bv3p.mongodb.net/?retryWrites=true&w=majority";
 
 main()
   .then(() => {
@@ -19,12 +18,12 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({}); // Clears any existing data
-  
+
   // Map through the data to add an owner to every sample listing
   // Replace the ID below with your actual User ID from MongoDB
   const updatedData = initData.data.map((obj) => ({
     ...obj,
-    owner: "69ba057210cacf468163d083", // YOUR USER ID HERE
+    owner: "6a26122d2723f2e7f38db98b",
   }));
 
   await Listing.insertMany(updatedData);
